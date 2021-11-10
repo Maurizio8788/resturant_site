48f3601369cee58ead53637c4f2d746674d6f457
Maurizio <mrz.strazzullo@gmail.com>
2021-11-10 22:27:57 +0100

    Commit iniziale

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header />
+  <Hero />
+  <Footer />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Layout/Header.vue";
+import Footer from "./components/Layout/Footer.vue";
+import Hero from "./components/sections/Hero.vue";
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   Header,
+   Hero,
+   Footer
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
